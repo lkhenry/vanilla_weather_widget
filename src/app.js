7131b630f1880d5b1257dd659a5eee18d058a728
lkhenry <82606240+lkhenry@users.noreply.github.com>
2021-09-02 03:14:49 -0400

    Update emoji, added search

--- src/app.js
+++ src/app.js
@@ -21,16 +21,32 @@ function displayTemp(response){
     let humidityElement=document.querySelector("#nowHum");
     let windElement=document.querySelector("#nowWind");
     let dateElement=document.querySelector("#current-dts");
+    let emojiElement=document.querySelector("#currentEmoji");
+
     temperatureElement.innerHTML=Math.round(response.data.main.temp);
     cityElement.innerHTML=response.data.name;
     descriptionElement.innerHTML=response.data.weather[0].description;
     humidityElement.innerHTML=response.data.main.humidity;
     windElement.innerHTML=Math.round(response.data.wind.speed);
     dateElement.innerHTML=formatDate(response.data.dt *1000);
+    emojiElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
-let apiKey="76d821d1d2a2ea449995354a4c66a73d";
+function search(city){
+    let apiKey="76d821d1d2a2ea449995354a4c66a73d";
 let units="metric";
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=Indianapolis&appid=${apiKey}&units=${units}`;
 
 axios.get(apiUrl).then(displayTemp);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement=document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("Indianapolis");
+
+let form=document.querySelector("#search-form");
+form.addEventListener("submit", search);

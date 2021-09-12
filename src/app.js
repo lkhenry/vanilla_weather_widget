a2ebc73f69594302bf42d1a0319c3cb1bf91a7f5
lkhenry <82606240+lkhenry@users.noreply.github.com>
2021-09-12 17:38:48 -0400

    Added forecast API

--- src/app.js
+++ src/app.js
@@ -13,23 +13,39 @@ function formatDate(timestamp){
     return `${day} ${hours}:${minutes}`; 
 }
 
-function displayForecast(){
-    let forecastElement=document.querySelector("#forecast") 
-      let days=["Mon", "Tues", "Wed", "Thu", "Fri"];
+function formatDay(timestamp){
+    let date=new Date(timestamp*1000);
+    let day=date.getDay();
+    let days=["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+}
+
+function displayForecast(response){
+    let forecast=response.data.daily;
+    let forecastElement=document.querySelector("#forecast"); 
+      
     let forecastHTML=`<div class="row">`;
-    days.forEach(function(day){
+    forecast.forEach(function(forecastDay, index){
+        if (index<6){
         forecastHTML= forecastHTML+` 
           <div class ="col-2">
-            <div class="weatherForecastDay" id="dayOne">${day}</div>
-            <img class= "weatherForecastIcon" id="dayOneIcon" src="https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png"/>
+            <div class="weatherForecastDay" id="dayOne">${formatDay(forecastDay.dt)}</div>
+            <img class= "weatherForecastIcon" id="dayOneIcon" src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" width="42"/>
             <div class= "weatherForecastTemp">
-              <span class="weatherForecastHigh" id="dayOneHigh">83°</span>
-               <span class="weatherForecastLow" id="dayOneLow">65°</span></div>
+              <span class="weatherForecastHigh" id="dayOneHigh">${Math.round(forecastDay.temp.max)}°</span>
+               <span class="weatherForecastLow" id="dayOneLow">${Math.round(forecastDay.temp.min)}°</span></div>
           </div>`;
-    })
+        }
+    });
     forecastHTML=forecastHTML+`</div>`;
     forecastElement.innerHTML=forecastHTML;
-        
+}    
+
+function getForecast(coordinates){
+    console.log(coordinates);
+    let apiKey="76d821d1d2a2ea449995354a4c66a73d";
+    let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${apiKey}`;
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemp(response){
@@ -51,6 +67,8 @@ function displayTemp(response){
     windElement.innerHTML=Math.round(response.data.wind.speed);
     dateElement.innerHTML=formatDate(response.data.dt *1000);
     emojiElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    getForecast(response.data.coord);
+
 }
 
 function search(city){
@@ -71,7 +89,7 @@ form.addEventListener("submit", handleSubmit);
 
 function displayFarenheit(event){
     event.preventDefault();
-    let farenheitTemperature=(17*9) /5 +32;
+    let farenheitTemperature=(celsiusTemperature*9) /5 +32;
     let temperatureElement=document.querySelector("#currentTemp");
     celsiusLink.classList.remove("active");
     farenheitLink.classList.add("active");
@@ -95,4 +113,3 @@ let farenheitLink=document.querySelector("#farenheit");
 farenheitLink.addEventListener("click", displayFarenheit);
 
 search("Indianapolis");
-displayForecast();
